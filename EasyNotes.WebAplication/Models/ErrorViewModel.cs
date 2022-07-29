d23e6fa563347adbcb0645113f5e76672395709f
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-07-29 21:44:18 +0300

    Delete EasyNotes.WebAplication directory
    unused

--- EasyNotes.WebAplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EasyNotes.WebAplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
